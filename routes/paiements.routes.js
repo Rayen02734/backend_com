d00ea871f71e6e44f0460751b282dfbad808f4af
Rayen02734 <sebairayen2006@gmail.com>
2026-09-16 13:56:59 +0100

    .

--- routes/paiements.routes.js
+++ routes/paiements.routes.js
@@ -7,7 +7,7 @@ router.get('/', paiementController.getPaiements);
 router.get('/:id', paiementController.getPaiementById);
 router.put('/:id', paiementController.updatePaiement);
 router.delete('/:id', paiementController.deletePaiement);
-router.put('/:id/effectuer', paiementController.effectuerPaiement);
+router.get('/:id/effectuer', paiementController.effectuerPaiement);
 router.get('/:id/verifier', paiementController.verifierPaiement);
 router.put('/:id/rembourser', paiementController.rembourser);
 
